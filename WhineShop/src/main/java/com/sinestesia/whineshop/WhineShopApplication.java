de31503e654252791960a0e72c68e57c521a6488
juanpibu99 <79905616+juanpibu99@users.noreply.github.com>
2022-07-26 11:01:44 +0200

    Add files via upload

---
+++ WhineShop/src/main/java/com/sinestesia/whineshop/WhineShopApplication.java
@@ -0,0 +1,13 @@
+package com.sinestesia.whineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhineShopApplication.class, args);
+	}
+
+}
---
+++ WhineShop/src/test/java/com/sinestesia/whineshop/WhineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinestesia.whineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
